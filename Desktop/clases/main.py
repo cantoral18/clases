03cbdb593d7df71a51e2c927deb9ef28b27ff058
cantoral18 <cantoralmaritza6@gmail.com>
2023-09-20 12:45:18 -0500

    ejercicios

--- Desktop/clases/main.py
+++ Desktop/clases/main.py
@@ -1,234 +1,260 @@
-# tipos de datos primitivos 
-"a" # string cadena texto 
-"hola" # esto tambien es un string
-"hola soy un string y te saludo" # cadena larga 
-## OBSERVACION: todo lo q este entre comillas es un string
-"100"
-"false"
-"hola"
-# un string puede estar entre comillas simples o dobles 
-100 ## este es un tipo de dato numerico entero 
-2.2 ## este es un tipo de dato numerico de tipo flotante float 
-#false ## este es un tipo booleano falso
-#true ## tipo de dato booleano verdadero 
+# # tipos de datos primitivos 
+# "a" # string cadena texto 
+# "hola" # esto tambien es un string
+# "hola soy un string y te saludo" # cadena larga 
+# ## OBSERVACION: todo lo q este entre comillas es un string
+# "100"
+# "false"
+# "hola"
+# # un string puede estar entre comillas simples o dobles 
+# 100 ## este es un tipo de dato numerico entero 
+# 2.2 ## este es un tipo de dato numerico de tipo flotante float 
+# #false ## este es un tipo booleano falso
+# #true ## tipo de dato booleano verdadero 
 
-# variables
-# es donde almacenamos nuestro tipo de dato
-# esos datos pueden mutar cambiar modificarse
-## como creamos una variable para almacenar nuestros datos
-#1. darle un nombre significativo o relacionado al dato que estamos almacenando 
-#2. indicarle a que dato esta relacionado -> asignacion =
-#3. indicar el tipo de dato que se va almacenar -> darle el dato a guardar 
-# primero el dato voy a pedir la edad de yadira 
-edad_yadira=18
-##el nombre de un alumno 
-nombre_alumno="yadira"
+# # variables
+# # es donde almacenamos nuestro tipo de dato
+# # esos datos pueden mutar cambiar modificarse
+# ## como creamos una variable para almacenar nuestros datos
+# #1. darle un nombre significativo o relacionado al dato que estamos almacenando 
+# #2. indicarle a que dato esta relacionado -> asignacion =
+# #3. indicar el tipo de dato que se va almacenar -> darle el dato a guardar 
+# # primero el dato voy a pedir la edad de yadira 
+# edad_yadira=18
+# ##el nombre de un alumno 
+# nombre_alumno="yadira"
 
-## operadores 
-#1. operadores aritmeticos 
-#suma +
-#resta - 
-#multiplicacion *
-#divicion /
-#suma
-suma=45+12
-#resta
-resta=45-12
-#multi
-multi=2*5
-#divi
-divi=4/2
-operacion=(45+12+23)/4
-op=15+12+14+13+/4
-# precedencia de operadores 
+# ## operadores 
+# #1. operadores aritmeticos 
+# #suma +
+# #resta - 
+# #multiplicacion *
+# #divicion /
+# #suma
+# suma=45+12
+# #resta
+# resta=45-12
+# #multi
+# multi=2*5
+# #divi
+# divi=4/2
+# operacion=(45+12+23)/4
+# op=15+12+14+13+/4
+# # precedencia de operadores 
 
-#operadores de uso especial 
-suma=45+42 #operador suma resultado 87
-suma="45"+12 #cocatenacion holamundo
-saludo="hola"+"mundo"#concatenacion holamundo 
-saludo="hola"+" "+"mundo"#concatenar hola mundo 
-saludos="hola"*2 #holahola
+# #operadores de uso especial 
+# suma=45+42 #operador suma resultado 87
+# suma="45"+12 #cocatenacion holamundo
+# saludo="hola"+"mundo"#concatenacion holamundo 
+# saludo="hola"+" "+"mundo"#concatenar hola mundo 
+# saludos="hola"*2 #holahola
 
-#datos estructurados 
-## listas
-# puede almacenar distintos tipos de datos en una sola lista separados por comas 
-lista=["nombre",10,False]
-lista_amigos=["yadira","medina","azurza","chinita"]
-## objetos
-#tambien al igual que las listas almacenan distintos tipos de datos pero con un orden 
-#para almacenar datos en un objeto necesitamos especificar un indice y un valor clave -> valor
-{
-    "nombre":"jory", 
-    "edad":52,
-    "sexo":"tos los dias "
- }
-#combinar ambas estructuras de datos 
-alumno0={
-    "nombre":"jori",
-    "edad":30,
-    "amigos":["antony","edwin","china"],
-    "direccion":{
-        "departamento":"ayacucho",
-        "provincia":"lucanas",
-        "distrito":"puquio",
-        "jiron":"san pedro",
-        "numero":152
-    }
-}
-alumnos=[{},{},{}]
+# #datos estructurados 
+# ## listas
+# # puede almacenar distintos tipos de datos en una sola lista separados por comas 
+# lista=["nombre",10,False]
+# lista_amigos=["yadira","medina","azurza","chinita"]
+# ## objetos
+# #tambien al igual que las listas almacenan distintos tipos de datos pero con un orden 
+# #para almacenar datos en un objeto necesitamos especificar un indice y un valor clave -> valor
+# {
+#     "nombre":"jory", 
+#     "edad":52,
+#     "sexo":"tos los dias "
+#  }
+# #combinar ambas estructuras de datos 
+# alumno0={
+#     "nombre":"jori",
+#     "edad":30,
+#     "amigos":["antony","edwin","china"],
+#     "direccion":{
+#         "departamento":"ayacucho",
+#         "provincia":"lucanas",
+#         "distrito":"puquio",
+#         "jiron":"san pedro",
+#         "numero":152
+#     }
+# }
+# alumnos=[{},{},{}]
 
-# controles de flujo
-## desiciones
-### solo se ejecuta el codigo si cumple o si la condicion es verdaderapodemos hacer si la condicion sea falasa ejecute otro codigo
-#### primero especificar el codigo si cumple una condicion
-#_______________________________________________________________________
-if <condicion>:
-    ## el codigo que deseamos ejecutar si la condicion es verdadera
-    print ("ejecuta esto")
-##aqui estamos fuera del if o del si este codigo siempre se ejecutara no depende de if
-print("esto siempre ejecuta")
-#_______________________________________________________________________
+# # controles de flujo
+# ## desiciones
+# ### solo se ejecuta el codigo si cumple o si la condicion es verdaderapodemos hacer si la condicion sea falasa ejecute otro codigo
+# #### primero especificar el codigo si cumple una condicion
+# #_______________________________________________________________________
+# if <condicion>:
+#     ## el codigo que deseamos ejecutar si la condicion es verdadera
+#     print ("ejecuta esto")
+# ##aqui estamos fuera del if o del si este codigo siempre se ejecutara no depende de if
+# print("esto siempre ejecuta")
+# #_______________________________________________________________________
 
-#si queremos que se ejecute un codigo en caso que sea falso
-if <condicion falsa>:
-    print("solo imprime si es verdad")
-else:
-    print("solo imprime si es falso")
+# #si queremos que se ejecute un codigo en caso que sea falso
+# if <condicion falsa>:
+#     print("solo imprime si es verdad")
+# else:
+#     print("solo imprime si es falso")
     
-###EJEMPLO
-if 15>18:
-    print("si es verdad imprime esto")
-else:
-    print("si es falso imprime esto")
-#######################################    
-if 15*2:
-    print("si es verdad imprime esto")
-else:
-    print("si es falso imprime esto")  
-#######################################
-condicion=True
-if condicion:
-    print("si es verdad imprime esto")
-else:
-    print("si es falso imprime esto")  
+# ###EJEMPLO
+# if 15>18:
+#     print("si es verdad imprime esto")
+# else:
+#     print("si es falso imprime esto")
+# #######################################    
+# if 15*2:
+#     print("si es verdad imprime esto")
+# else:
+#     print("si es falso imprime esto")  
+# #######################################
+# condicion=True
+# if condicion:
+#     print("si es verdad imprime esto")
+# else:
+#     print("si es falso imprime esto")  
 
-##ciclos
-## existen dos tipos
-###  cuando sabes la cantidad de  veces que vamos a repetir
-# para este caso existe el for
-#sintaxis despues de la palabra reservada for  debemos crear una variable que 
-# almacene el numero que iremos iterando.
-#luego tendremos que indicar  el rango a iterar alos elementos a iterar
-vocales=("a","e","i","o","u")
-for i in vocales:
-    print(i)
+# ##ciclos
+# ## existen dos tipos
+# ###  cuando sabes la cantidad de  veces que vamos a repetir
+# # para este caso existe el for
+# #sintaxis despues de la palabra reservada for  debemos crear una variable que 
+# # almacene el numero que iremos iterando.
+# #luego tendremos que indicar  el rango a iterar alos elementos a iterar
+# vocales=("a","e","i","o","u")
+# for i in vocales:
+#     print(i)
 
 
-### cuando no sabemos la cantidad de veces a repetir
-while  tambien evalua si la condision es verdadera
-while true:
-    print(hola) 
+# ### cuando no sabemos la cantidad de veces a repetir
+# while  tambien evalua si la condision es verdadera
+# while true:
+#     print(hola) 
     
-condicion= true 
-while condicion:
-    print("hola")
-    texto=input ( "ingresa tu nombre o salir para terminar el programa:")
-    if texto =="salir":
-        condicion=false
+# condicion= true 
+# while condicion:
+#     print("hola")
+#     texto=input ( "ingresa tu nombre o salir para terminar el programa:")
+#     if texto =="salir":
+#         condicion=false
         
         
- # ##  esta funcion nos muestra el numero mayor de una lista
-# #lista=[45,23,22,3,21]
-# numero_mayor=max(lista)
-# print(numero_mayor)
+#  # ##  esta funcion nos muestra el numero mayor de una lista
+# # #lista=[45,23,22,3,21]
+# # numero_mayor=max(lista)
+# # print(numero_mayor)
 
-# ## esta funcion nos muestra el numero menor de una lista 
-# lista=[45,23,22,3,21]
-# numero_menor=min(lista)
-# print(numero_menor)
+# # ## esta funcion nos muestra el numero menor de una lista 
+# # lista=[45,23,22,3,21]
+# # numero_menor=min(lista)
+# # print(numero_menor)
 
-# funciones para convertir de un string a numero entero int("100")#->>100->> entero
+# # funciones para convertir de un string a numero entero int("100")#->>100->> entero
 
-# funcion para convertir un entero a un strimg
-# str(100) #->> "100"->>string   
+# # funcion para convertir un entero a un strimg
+# # str(100) #->> "100"->>string   
 
-# funcion de python que nos permite agregar elementos al final de una lista 
-# lista=[15,12,78]
-# elemento=100
-# lista.append(elemento)
-# print(lista) 
+# # funcion de python que nos permite agregar elementos al final de una lista 
+# # lista=[15,12,78]
+# # elemento=100
+# # lista.append(elemento)
+# # print(lista) 
    
-# # funcion que nos permite eliminar los elementos que se encuentran al final de una lista
-# lista=[15,12,78]
-# lista.pop()
+# # # funcion que nos permite eliminar los elementos que se encuentran al final de una lista
+# # lista=[15,12,78]
+# # lista.pop()
+# # print(lista)
+# # print (eliminado)
+
+# # #funcion de python que nos permite agregar elementos en cualquier  posicion de mi 
+# # lista para eso se le tiene que pasar dos parametros
+# # ,primero indicar el indice y segundo el datoque se va agregar   
+
+# # lista_monbres=["jory","luis","pepe"]
+# # lista_monbres.inst(1,"carlos")
+# # print(lista_monbres)
+
+# # funcion de python que nos permite eliminar elementos de cualquier
+# # posicion de una lista ,esta funcion recibe solo el elemento que deseamos eliminar
+
+# lista=[4,3,5,6,8]
+# lista.remove(6)
+# print (lista)
+
+# #funcion que nos permite dividir en un acadena
+# cadena="hola como estan"
+# lista=cadena.split()
 # print(lista)
-# print (eliminado)
+# url="www.golle.com/id=70133573"
+# id=url.splip("=").pop()
+# print(id)
 
-# #funcion de python que nos permite agregar elementos en cualquier  posicion de mi 
-# lista para eso se le tiene que pasar dos parametros
-# ,primero indicar el indice y segundo el datoque se va agregar   
+# # ### 2. FUNCIONES CREADAS
+# # FUNCIONES PROPIAS
+# # PASOS PARA CREAR UNA FUNCION PROPIA
+# # 1. hacer uso de la palabra reservada
+# # 2. defenir un nombre de funcion que describa que tarea va a realizar
+# # 3. establecer los parametros que resivira la funcion entre parentisis()
+# # 4. establecer que valor o dato va retornar mi funcion con la palabra reservada return
+# # >observacion =>> tambien podemos hacer uso de la funcion print (para retornar un mensaje en nuestra funcion)
+# # retornar un mensaje en nuestra function
+# # existen 2 tipos de funciones los que resiven ningun parametro
+# # y los que resven prarmetro
+# # def saludo()
+# #     print("hola este es un saludo")
 
-# lista_monbres=["jory","luis","pepe"]
-# lista_monbres.inst(1,"carlos")
-# print(lista_monbres)
+# # como hacemos uso de la function
+# # nombre de la funciones y parentesis
 
-# funcion de python que nos permite eliminar elementos de cualquier
-# posicion de una lista ,esta funcion recibe solo el elemento que deseamos eliminar
-
-lista=[4,3,5,6,8]
-lista.remove(6)
-print (lista)
-
-#funcion que nos permite dividir en un acadena
-cadena="hola como estan"
-lista=cadena.split()
-print(lista)
-url="www.golle.com/id=70133573"
-id=url.splip("=").pop()
-print(id)
-
-# ### 2. FUNCIONES CREADAS
-# FUNCIONES PROPIAS
-# PASOS PARA CREAR UNA FUNCION PROPIA
-# 1. hacer uso de la palabra reservada
-# 2. defenir un nombre de funcion que describa que tarea va a realizar
-# 3. establecer los parametros que resivira la funcion entre parentisis()
-# 4. establecer que valor o dato va retornar mi funcion con la palabra reservada return
-# >observacion =>> tambien podemos hacer uso de la funcion print (para retornar un mensaje en nuestra funcion)
-# retornar un mensaje en nuestra function
-# existen 2 tipos de funciones los que resiven ningun parametro
-# y los que resven prarmetro
-# def saludo()
-#     print("hola este es un saludo")
-
-# como hacemos uso de la function
-# nombre de la funciones y parentesis
-
-# funcion con parametros
-def mi_print(texto)
-    print(texto)
+# # funcion con parametros
+# def mi_print(texto)
+#     print(texto)
     
 
  
-#  print("hola este es print de python")
-#  mi_print("hola este es mi print creado")
-def suma(a,b)
- total=a+b
- return total
- mi_print(suma(45,12)) ##==>>>>> 57
+# #  print("hola este es print de python")
+# #  mi_print("hola este es mi print creado")
+# def suma(a,b)
+#  total=a+b
+#  return total
+#  mi_print(suma(45,12)) ##==>>>>> 57
  
 #########################################
  #ejemplo 
 #  para que se usa esta funcion 
 #  para mostrar` el valor maximo de una lista
  
- lista=[12,4,43,56,78]
- mi_print(max(lista)) #===>>>78
+#  lista=[12,4,43,56,78]
+#  mi_print(max(lista)) #===>>>78
  
-def mi_max(lista):
-    numero_mayor=lista[0]
-    for numero in lista:
-        if numero> numero_mayor:
-    return numero_mayor
-mi_print(mi_max(lista))
+# def mi_max(lista):
+#     numero_mayor=lista[0]
+#     for numero in lista:
+#         if numero> numero_mayor:
+#             numero_mayor=numero
+#     return numero_mayor
+# mi_print(mi_max(lista))
+    
+# def mi_min(lista):
+#     numero_menor=lista[0]
+#     for numero in lista:
+#         if numero> numero_menor:
+#             numero_menor=numero
+#     return numero_menor
+# mi_print(mi_min(lista))
+
+# #funcion es con muchos parametros
+
+# def funciones(*muchos_parametros):
+#     total=0
+#     for numero in muchos_parametros:
+#         total=total+numero
+#     return total
+# print( funciones(45,52,85,96))
+
+def datos(*args):
+    nombre=args[0]
+    apellido=args[1]
+    edad=args[2]
+    return f"mi nombre es ,{nombre},{apellido}y mi edad es,{edad}"
+print(datos("luis","apellido","50"))
+    
     
--- Desktop/clases/repaso/ejercicio.py
+++ Desktop/clases/repaso/ejercicio.py
@@ -1,4 +1,4 @@
-#CREAR UN PROGRAMA QUE ME PIDA LA EDAD DE UNA PERSONA SI LA EDAD ES MAYOR O IGUAL
+#1.CREAR UN PROGRAMA QUE ME PIDA LA EDAD DE UNA PERSONA SI LA EDAD ES MAYOR O IGUAL
 # A 18 QUE MUESTRE UN MENSAJE "ERES MAYOR DE EDAD"CASO CONTRARIO QUE 
 # MUESTRE UN MENSAJE "ERES MENOR DE EDAD"
 
@@ -9,7 +9,7 @@
 #     print("eres menor de edad")
 
 
-# #UNA TIENDA COMERCIAL DESEA HACER UN DESCUENTO DEL 20% CREAR UN PROGRAMA QUE ME DETERMINE SIE EL
+# #2.UNA TIENDA COMERCIAL DESEA HACER UN DESCUENTO DEL 20% CREAR UN PROGRAMA QUE ME DETERMINE SIE EL
 # # CLIENTE SE HACE ACREADOR DEL DESCUENTO TENIENDO ENCUENTA LOS SGTS ,SI EL CLIENTE REALIZA UNA COMPRA DE IGUAL 
 # # O MAYOR A 1000 SOLES MOSTRAR UN MENSAJE QUE DIGA " GANASTES UN DESCUENTO DE 20% AHORA PAGARS
 # # < MOSTRAR EL TOTAL DE LA COMPRA MENOS EL DESCUENTO>" EN CASO LA COMPRA NO SUPERE LOS 1000 SOLES 
@@ -24,7 +24,7 @@
 #     print(f"No aplicas al descuento. El monto de la compra es de {monto_compra} soles.")
 
 
-#crear un programa que me pida 5 veces un nombre y por cada vez que 
+#3. crear un programa que me pida 5 veces un nombre y por cada vez que 
 #lo pida muestre la cantidad de veces que ingreso el nombre
 
 
@@ -32,18 +32,67 @@
 #     nombre=input("ingrese nombre:")
 #     print( f"ingresastes {n} veces el nombre")
 
-#crear un programa que pida un numero y lo evalue con el numero premiado si el numero
+#4 .crear un programa que pida un numero y lo evalue con el numero premiado si el numero
 #ingresado es premiado el programa facilicitara si el numero ingresado es 
 #incorrecto el programa seguira pidiendo el numero premiado
 
-numero_ganador=56
-condicion=True
-while condicion:
-    numero_ingresado=int(input("ingrese un numero :"))
-    if numero_ingresado == numero_ganador:
-        print ("ganastes")
-        condicion=False
+# numero_ganador=56
+# condicion=True
+# while condicion:
+#     numero_ingresado=int(input("ingrese un numero :"))
+#     if numero_ingresado == numero_ganador:
+#         print ("ganastes")
+#         condicion=False
+#     else:
+#         print("sihgue intentando")
+
+
+# 5.crear una funcion para cada operador aritmeticoy que resiva2 parametros y retorne el resultado
+# de la operacion ,ojo,crearse una funcion que nos permita imprimir el resultado.
+
+# def mi_print(texto):
+#     print(texto)
+    
+# def resta (a,b):
+#     return a-b
+# def suma(a,b):
+#     return a+b
+# def multi(a,b):
+#     return a*b
+# def divi(a,b):
+#     return a/b
+
+# mi_print(resta(14,1))
+# mi_print(suma(14,1))
+# mi_print(multi(14,1))
+# mi_print(divi(14,1))
+
+#ejercio 6
+#escriba una funcion que reciba un numero entero y devuelva su factorial
+
+def factorial(n):
+    if n ==0:
+        return 1
     else:
-        print("sihgue intentando")
+        resultado = 1
+        for i in range (1,n+1):
+            resultado= resultado
+            
+numero=10
+resultado=factorial(numero)
+print(f"el factorial de {numero} es {resultado}")
+
+#ejercicio 7
+# escribir una funcion que resiva como parametro una lista de numeros y 
+# retorne una nueva lista con el cuadrado de cada numeron de lista egresados
+
+lista=(2,8,6)
+def 
 
 
+#ejercicio 8
+## escribir un programa que reciba una cadaena de caracteres y desvela un
+# objeto con cada palabra que tiene  y su frecuencia
+
+
+    
