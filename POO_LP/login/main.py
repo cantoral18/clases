a658c35580cd1e09c3384c06837cea870e173d89
cantoral18 <cantoralmaritza6@gmail.com>
2023-10-25 13:05:04 -0500

    ususario y contraseña

--- POO_LP/login/main.py
+++ POO_LP/login/main.py
@@ -5,7 +5,7 @@ from bd import*#la variable usuario de mi bd estara disponibleen este archivo
 #esta clase  tendra los siguientes metodos
 #verificar si usuario esta registrado o existe en mis registro
 #validar usuario y password
-
+from bd import usuario
 
 class usuario:
     def verificar_usuario(self,usuario):
@@ -14,7 +14,11 @@ class usuario:
         print("si esta en el registro")
     else:
         print("no esta registrado")
-    def validar_u_p(self,usuario,password):
+    def verificar_u_p(self,usuario,password):
         if usuario in self.usuario:
             return self.usuario[usuario]==password
         return False
+    if verificar_u_p("usuario",password):
+        print(verificacion valida.)
+    else:
+        print("credenciales invalidas.")
