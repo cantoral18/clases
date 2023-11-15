04d7775c4a79fe83ffbf992812bc4a9b528498da
cantoral18 <cantoralmaritza6@gmail.com>
2023-11-15 08:49:34 -0500

    calculadora avanzada

--- interfaces_HDS/calculadora_avanzada.py/funciones.py
+++ interfaces_HDS/calculadora_avanzada.py/funciones.py
@@ -1,30 +1,38 @@
-from tkinter import*
+from tkinter import *
 def enviar_boton(ventana,valor):
-   if valor == "=":
-       print("soy igual")
-   elif valor=="C":
-       ventana.caja_operaciones_label.config(text="FSD")
-       ventana.operacion_label.delete(0,END)
-       
-   elif valor=="<":
-       valor_actual=ventana.caja_operaciones.get()
-       if valor_actual:
-           nuevo_valor=valor_actual[1-1]
-           ventana.caja_operaciones.delete(0,END)
-       
-   else:
-       print(f"soy el valor(valor)")
-       
-       
-def cambio_tema(ventana,colores):
-        if ventana.tema_oscuro:
-            ventana.configure(dg=colores.color_fondo_ligth)
-            ventana.caja_operaciones.config(fg=colores.color_texto_ligt,bg= colores.color_caja_texto_ligth)
-            ventana.operacion_label.config(fg=colores.color_texto_light,bg=colores.color_fondo_light)
-            ventana.boton_tema.configure(text="/uf185 modo claro",relief=sunken,bg=color.color_botiones_especial_light)
+    if valor == "=":
+        expresion=ventana.caja_operaciones.get().replace("%","/100")
+        resultado=eval(expresion)
+        ventana.caja_operaciones.delete(0,END)
+        ventana.caja_operaciones.insert(0,str(resultado))
+        operacion=expresion+" "+valor
+        ventana.operacion_label.config(text=operacion)
+    elif valor == "C":
+        ventana.operacion_label.config(text="")
+        ventana.caja_operaciones.delete(0,END)
+    elif valor == "<":
+        valor_actual=ventana.caja_operaciones.get()
+        if valor_actual:
+            nuevo_valor=valor_actual[:-1]
+            ventana.caja_operaciones.delete(0,END)
+            ventana.caja_operaciones.insert(0,nuevo_valor)
+            ventana.operacion_label.config(text=nuevo_valor+" ")
     else:
-            ventana.configure(dg=colores.color_fondo_negro)
-            ventana.caja_operaciones.config(fg=colores.color_texto_negro,bg= colores.color_caja_texto_negro)
-            ventana.operacion_label.config(fg=colores.color_texto_negro,bg=colores.color_fondo_negro)
-            ventana.boton_tema.configure(text="/uf185 modo oscuro",relief=sunken,bg=color.color_botiones_especial_negro)
-        ventana.tema_oscuro=not ventana. tema_oscuro
+        valor_actual=ventana.caja_operaciones.get()
+        ventana.caja_operaciones.delete(0,END)
+        ventana.caja_operaciones.insert(0,valor_actual+valor)
+        if valor == "=":
+            ventana.operacion_label.config(text="")
+
+def cambio_tema(ventana,colores):
+    if ventana.tema_oscuro:
+        ventana.configure(bg=colores.COLOR_FONDO_LIGHT)
+        ventana.caja_operaciones.config(fg=colores.COLOR_TEXTO_LIGHT,bg=colores.COLOR_CAJA_TEXTO_LIGHT)
+        ventana.operacion_label.config(fg=colores.COLOR_TEXTO_LIGHT,bg=colores.COLOR_FONDO_LIGHT)
+        ventana.boton_tema.configure(text="Modo Claro \uf185", relief=SUNKEN,bg=colores.COLOR_BOTONES_ESPECIALES_LIGHT)
+    else:
+        ventana.configure(bg=colores.COLOR_FONDO_NEGRO)
+        ventana.caja_operaciones.config(fg=colores.COLOR_TEXTO_NEGRO,bg=colores.COLOR_CAJA_TEXTO_NEGRO)
+        ventana.operacion_label.config(fg=colores.COLOR_TEXTO_NEGRO,bg=colores.COLOR_FONDO_NEGRO)
+        ventana.boton_tema.configure(text="Modo Oscuro \uf186", relief=SUNKEN,bg=colores.COLOR_BOTONES_ESPECIALES_NEGRO)
+    ventana.tema_oscuro=not ventana.tema_oscuro
